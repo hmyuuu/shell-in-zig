702f1d43e122c29268664538e1eec19dad2eaffa
Huai-Ming Yu <hmyuus@gmail.com>
2025-11-09 02:35:18 +0800

    codecrafters submit [skip ci]

--- src/main.zig
+++ src/main.zig
@@ -1,9 +1,17 @@
 const std = @import("std");
 
-var stdout_writer = std.fs.File.stdout().writerStreaming(&.{});
-const stdout = &stdout_writer.interface;
-
 pub fn main() !void {
-    // TODO: Uncomment the code below to pass the first stage
+    var stdout_buffer: [512]u8 = @splat(0);
+    var stdout_writer = std.fs.File.stdout().writerStreaming(&stdout_buffer);
+    const stdout = &stdout_writer.interface;
+    var stdin_buffer: [512]u8 = @splat(0);
+    var stdin_reader = std.fs.File.stdin().readerStreaming(&stdin_buffer);
+    const stdin = &stdin_reader.interface;
+
     try stdout.print("$ ", .{});
+    try stdout.flush();
+
+    const input = try stdin.takeDelimiterExclusive('\n');
+    try stdout.print("{s}: command not found\n", .{input});
+    try stdout.flush();
 }
